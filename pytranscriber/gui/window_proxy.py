6218f551991bd46c56f2c8b0ed6667948a3495f1
Raryel C. Souza <raryel.costa@gmail.com>
2023-03-10 01:12:17 -0300

    proxy gui package

--- pytranscriber/gui/window_proxy.py
+++ pytranscriber/gui/window_proxy.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\proxy.ui'
+# Form implementation generated from reading ui file '.\window_proxy.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
