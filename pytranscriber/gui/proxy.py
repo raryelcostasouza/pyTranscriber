59af5015b695206232c72c037b4c248318832476
Raryel C. Souza <raryel.costa@gmail.com>
2023-08-09 23:55:05 -0300

    proxy window

--- pytranscriber/gui/proxy.py
+++ pytranscriber/gui/proxy.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\window_proxy.ui'
+# Form implementation generated from reading ui file '.\proxy.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(381, 171)
+        Dialog.resize(500, 120)
         Dialog.setAutoFillBackground(False)
         Dialog.setSizeGripEnabled(False)
         self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
@@ -30,9 +30,6 @@ class Ui_Dialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.radioButtonNone.sizePolicy().hasHeightForWidth())
         self.radioButtonNone.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        self.radioButtonNone.setFont(font)
         self.radioButtonNone.setChecked(True)
         self.radioButtonNone.setObjectName("radioButtonNone")
         self.verticalLayout_2.addWidget(self.radioButtonNone)
@@ -43,9 +40,6 @@ class Ui_Dialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.radioButtonHTTP.sizePolicy().hasHeightForWidth())
         self.radioButtonHTTP.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        self.radioButtonHTTP.setFont(font)
         self.radioButtonHTTP.setObjectName("radioButtonHTTP")
         self.verticalLayout_2.addWidget(self.radioButtonHTTP)
         self.gridLayout = QtWidgets.QGridLayout()
@@ -57,32 +51,34 @@ class Ui_Dialog(object):
         self.lineEditHttpProxy.setObjectName("lineEditHttpProxy")
         self.gridLayout.addWidget(self.lineEditHttpProxy, 0, 1, 1, 1)
         self.label = QtWidgets.QLabel(self.groupBox)
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        self.label.setFont(font)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.bSave = QtWidgets.QPushButton(self.groupBox)
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        self.bSave.setFont(font)
-        self.bSave.setObjectName("bSave")
-        self.gridLayout.addWidget(self.bSave, 1, 2, 1, 1)
+        self.pushButtonTest = QtWidgets.QPushButton(self.groupBox)
+        self.pushButtonTest.setEnabled(True)
+        self.pushButtonTest.setObjectName("pushButtonTest")
+        self.gridLayout.addWidget(self.pushButtonTest, 0, 2, 1, 1)
         self.verticalLayout_2.addLayout(self.gridLayout)
         self.verticalLayout.addWidget(self.groupBox)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
         self.radioButtonNone.clicked['bool'].connect(self.lineEditHttpProxy.setDisabled)
         self.radioButtonNone.clicked['bool'].connect(self.pushButtonTest.setDisabled)
+        self.radioButtonHTTP.clicked['bool'].connect(self.pushButtonTest.setEnabled)
         self.radioButtonHTTP.clicked['bool'].connect(self.lineEditHttpProxy.setEnabled)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Proxy setting"))
-        self.radioButtonNone.setText(_translate("Dialog", "Disabled"))
-        self.radioButtonHTTP.setText(_translate("Dialog", "Enabled"))
-        self.pushButtonTest.setText(_translate("Dialog", "Test"))
+        self.radioButtonNone.setText(_translate("Dialog", "None"))
+        self.radioButtonHTTP.setText(_translate("Dialog", "HTTP"))
         self.lineEditHttpProxy.setPlaceholderText(_translate("Dialog", "http://127.0.0.1:1080"))
         self.label.setText(_translate("Dialog", "URL:"))
-        self.bSave.setText(_translate("Dialog", "Save"))
+        self.pushButtonTest.setText(_translate("Dialog", "Test"))
