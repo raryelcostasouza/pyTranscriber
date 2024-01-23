a99daa12ecdc4d09662620a170177eb0c9149474
Raryel C. Souza <raryel.costa@gmail.com>
2024-01-23 00:40:03 -0300

    Parametrized DB functions

--- pytranscriber/control/ctr_db.py
+++ pytranscriber/control/ctr_db.py
@@ -4,6 +4,7 @@ import sqlite3
 
 class CtrDB:
     conn = None
+    DB_ERROR = "DB Error"
 
     def connect(self):
         if self.conn:
@@ -14,37 +15,52 @@ class CtrDB:
                 cur = self.conn.cursor()
                 return cur
             except Exception as ex:
-                MessageUtil.show_error_message(str(ex))
+                MessageUtil.show_error_message(str(ex), self.DB_ERROR)
                 return None
 
     def close(self):
         self.conn.close()
         self.conn = None
 
-    def save_license_data(self, license_data):
+    def _load_one_row(self, table_name):
         cur = self.connect()
         try:
-            cur.execute('INSERT INTO License (expiry,plan,key) VALUES (?, ?, ?)',
-                        (license_data.expiry, license_data.plan, license_data.key))
-            self.conn.commit()
-        except sqlite3.Error as e:
-            MessageUtil.show_error_message(str(e))
-        self.close()
-
-    def clear_license_data(self):
-        cur = self.connect()
-        try:
-            cur.execute('DELETE FROM License')
-            self.conn.commit()
-        except sqlite3.Error as e:
-            MessageUtil.show_error_message(str(e))
-        self.close()
-
-    def load_license_data(self):
-        cur = self.connect()
-        try:
-            cur.execute('SELECT * FROM License')
+            cur.execute('SELECT * FROM ' + table_name)
             return cur.fetchone()
         except sqlite3.Error as e:
-            MessageUtil.show_error_message(str(e))
+            MessageUtil.show_error_message(str(e), self.DB_ERROR)
             return None
+
+    def _save_single_column(self, query, value):
+        cur = self.connect()
+        try:
+            cur.execute(query,(value,))
+            self.conn.commit()
+        except sqlite3.Error as e:
+            MessageUtil.show_error_message(str(e), self.DB_ERROR)
+        self.close()
+
+    def _truncate_table(self, table_name):
+        cur = self.connect()
+        try:
+            cur.execute('DELETE FROM ' + table_name)
+            self.conn.commit()
+        except sqlite3.Error as e:
+            MessageUtil.show_error_message(str(e), self.DB_ERROR)
+        self.close()
+
+    def load_last_language(self):
+        return self._load_one_row('Language')
+
+    def clear_last_language(self):
+        self._truncate_table('Language')
+
+    def save_last_language(self, language):
+        cur = self.connect()
+        try:
+            cur.execute('INSERT INTO Language (last_language) VALUES (?)',
+                        (language,))
+            self.conn.commit()
+        except sqlite3.Error as e:
+            MessageUtil.show_error_message(str(e), self.DB_ERROR)
+        self.close()
