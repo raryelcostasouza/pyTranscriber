2bb691ae32f382eb798015776882a9bcce4164d4
Raryel C. Souza <raryel.costa@gmail.com>
2025-03-04 11:58:20 -0300

    patch freeze scripts

--- freeze-linux.sh
+++ freeze-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pipenv shell
-pyinstaller main.py --path="$(pwd)" --add-binary="ffmpeg:." --add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" --onefile --clean
+pyinstaller main.py main.spec --path="$(pwd)" --add-binary="ffmpeg:." --add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" --onefile --clean
--- freeze-mac.sh
+++ freeze-mac.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 pipenv shell
-pyinstaller main.py --path="$(pwd)" --add-binary="ffmpeg:." --add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" --clean --windowed
+pyinstaller main.py \
+--path="$(pwd)" \
+--add-binary="ffmpeg:." \
+--add-binary="pytranscriber.sqlite:." \
+--add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" \
+--add-data="venv/lib/python3.8/site-packages/whisper/assets:whisper/assets" \
+--clean  \
+--windowed \
+--noconfirm \
+--clean \
+#--osx-entitlements-file="mac/entitlements.plist"
