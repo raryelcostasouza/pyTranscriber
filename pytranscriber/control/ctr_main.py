01ba3762fa02373de8f5f24ff7d2f4dbde55aa0d
Raryel C. Souza <raryel.costa@gmail.com>
2025-03-08 15:28:46 -0300

    Added GPL copyright notice

--- pytranscriber/control/ctr_main.py
+++ pytranscriber/control/ctr_main.py
@@ -1,5 +1,5 @@
 '''
-   (C) 2019 Raryel C. Souza
+   (C) 2025 Raryel C. Souza
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
