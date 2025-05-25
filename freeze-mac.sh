a4bb75c7a64c63d9a726eacbc98310837e836748
Raryel C. Souza <raryel.costa@gmail.com>
2025-05-24 21:45:38 -0300

    final fixes mac freeze local script

--- freeze-mac.sh
+++ freeze-mac.sh
@@ -3,12 +3,11 @@
 pipenv shell
 pyinstaller main.py \
 --path="$(pwd)" \
---add-binary="ffmpeg:." \
+--add-binary="ffmpeg-bin/ffmpeg:." \
 --add-binary="pytranscriber.sqlite:." \
 --add-data="pytranscriber/gui/*.qm:pytranscriber/gui/" \
 --add-data="venv/lib/python3.8/site-packages/whisper/assets:whisper/assets" \
 --clean  \
 --windowed \
---noconfirm \
---clean \
-#--osx-entitlements-file="mac/entitlements.plist"
+--noconfirm 
+
